fe5427fa249be696313729e443fabc1f5853895d
arun <arungovindn010@gmail.com>
2021-08-16 13:22:10 +0530

    Remove unnecessary codes

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Client side JS is loaded')
-
-
-
 const weatherform =document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#Message1')
