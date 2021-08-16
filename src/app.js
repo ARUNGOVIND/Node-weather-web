e7e13b79693855ba9ce3946a427b84ce217707c7
arun <arungovindn010@gmail.com>
2021-08-16 16:25:10 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.get('/about',(req,res)=>{
 app.get('/help',(req,res)=>{
     res.render('help',{
         title:'Help',
-        helptext:'This is some helpful text',
+        helptext:'Test app',
         name:'Arun'
     })  
 })
